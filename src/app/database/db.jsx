a8c48fbf1f6ff85f53fa27caebcd1fa225ff08f2
Piyush Giri <piyush.giri232003@gmail.com>
2024-01-22 10:32:46 +0530

    Installed mongodb package and created database/db.js

---
+++ src/app/database/db.jsx
@@ -0,0 +1,28 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri = process.env.MONGO_URI
+const user=process.env.MONGO_ATLAS_ID
+const password=process.env.MONGO_ATLAS_PASSWORD
+const atlas_uri=`mongodb+srv://${user}:${password}@cluster0.3m4yenq.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function connectDB() {
+  try {
+    await client.connect();
+    
+    const database = client.db('TypingMaster');
+    const users = database.collection('Users');
+    const passage=database.collection('Passage');
+    
+    return {users,passage};
+  } catch (err) {
+    console.error('Error connecting to database:', err);
+    throw err;
+  }
+}
+
+export default connectDB;
