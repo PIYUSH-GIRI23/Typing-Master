23f895e58f67cbc8528df7a7f17ce4d33d9c5ef2
Piyush Giri <piyush.giri232003@gmail.com>
2024-01-23 19:52:23 +0530

    Navbar created

---
+++ src/app/fullscreen/Navbar/navbar.js
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+import {styles} from '../fullscreen.css'
+export default function navbar() {
+  return (
+    <div>
+      hello , this is navbar
+    </div>
+  )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
-
+import Navbar from "./fullscreen/Navbar/navbar";
 export const metadata = {
   title: "RapidKeys",
   description: "Boost Your Typing Speed with RapidKeys - The Quickest Path to Keyboard Mastery!",
@@ -11,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
