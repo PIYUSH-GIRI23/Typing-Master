90acf097749fd0fcf2671a07674a0adf9b9641d6
Piyush Giri <piyush.giri232003@gmail.com>
2024-01-23 21:10:20 +0530

    Navbar element for fullscreen created

--- src/app/fullscreen/Navbar/navbar.js
+++ src/app/fullscreen/Navbar/navbar.js
@@ -1,10 +1,26 @@
 "use client"
 import React from 'react'
-import {styles} from '../fullscreen.css'
+import '../fullscreen.css'
+import Link from 'next/link'
 export default function navbar() {
   return (
-    <div>
-      hello , this is navbar
+    <div className="fullscreenNavbar">
+      <div className="fullscreenAboutus">
+        <Link href="#" className="fullscreenAboutusButton">About Us</Link>
+      </div >
+      <div className="fullscreenLoginNo">
+        <Link href="#" className="fullscreenSignupbutton">Signup</Link>
+        <Link href="#" className="fullscreenLoginbutton">Login</Link>
+      </div>
+      <div className="fullscreenLoginYes">
+        <div>
+          <Link href="#" className="fullscreenProfilebutton">
+            {/* your username thorugh backened */}
+            piyushgiri2310
+          </Link>
+          <span  className="fullscreenDropdownMenu">V</span>
+        </div>
+      </div>
     </div>
   )
 }
