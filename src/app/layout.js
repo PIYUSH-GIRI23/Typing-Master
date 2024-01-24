d07d1c5adf38c10efbc5f05d538e5e80b860ceb2
Piyush Giri <piyush.giri232003@gmail.com>
2024-01-24 09:14:58 +0530

    Downloaded logo.png and removed navbar from layout.js and adding it to page.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
-import Navbar from "./fullscreen/Navbar/navbar";
 export const metadata = {
   title: "RapidKeys",
   description: "Boost Your Typing Speed with RapidKeys - The Quickest Path to Keyboard Mastery!",
@@ -11,7 +10,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import Navbar from "./fullscreen/Navbar/navbar";
 export default function Home() {
   return (
     <main>
-      
+      <Navbar />
     </main>
   );
 }
