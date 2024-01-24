ba0d7366c704eda61991b59f6379c7438a0bebd0
Piyush Giri <piyush.giri232003@gmail.com>
2024-01-24 12:46:23 +0530

    completed navbar frontend (logic remaining for login/signup) changed css

--- src/app/fullscreen/Navbar/navbar.js
+++ src/app/fullscreen/Navbar/navbar.js
@@ -1,21 +1,31 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react';
 import '../fullscreen.css'
 import Link from 'next/link'
 export default function navbar() {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
   return (
     <div className="fullscreenNavbar">
-      <div className="fullscreenLoginNo">
+      {/* <div className="fullscreenLoginNo">
         <Link href="/fullscreen/signup" className="fullscreenSignupbutton">Signup</Link>
         <Link href="/fullscreen/login" className="fullscreenLoginbutton">Login</Link>
-      </div>
+      </div> */}
       <div className="fullscreenLoginYes">
         <div>
           <Link href="#" className="fullscreenProfilebutton">
             {/* your username thorugh backened */}
             piyushgiri2310
           </Link>
-          <span  className="fullscreenDropdownMenu">V</span>
+          <span  className="fullscreenDropdownMenu" onClick={()=>setDropdownVisible(!isDropdownVisible)}>V</span>
+          {isDropdownVisible && (
+            <div className="fullscreendropdownContent">
+              <Link href="#" className="fullscreendropdownchild">Account Details</Link>
+              <hr className="hr"/>
+              <Link href="#" className="fullscreendropdownchild">Statistics</Link> 
+              <hr className="hr"/>
+              <div className="fullscreendropdownchild" id="logout">Logout</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
