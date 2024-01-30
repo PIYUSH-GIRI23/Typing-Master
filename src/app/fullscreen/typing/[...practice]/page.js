f9e9b753008b972c6b206526aa17d1797fde6b77
Piyush Giri <piyush.giri232003@gmail.com>
2024-01-30 18:07:36 +0530

    i will write proper code tomorrrow:

--- src/app/fullscreen/typing/[...practice]/page.js
+++ src/app/fullscreen/typing/[...practice]/page.js
@@ -7,15 +7,6 @@ import Image from 'next/image';
 const Page = ({ params }) => {
   const router = useRouter();
   const [showContent, setShowContent] = useState(false)
-  const [passage, setpassage] = useState([])
-  const [donotreload, setDonotreload] = useState(false)
-  const [whichpassage,setwhichpassage]  = useState(0)
-  const [particularpassage,setparticularpassage] = useState([])
-
-  const [charcounter, setCharcounter] = useState(0)
-  const [wordcounter, setWordcounter] = useState(0)
-  const [rowcounter, setRowcounter] = useState(0)
-  const [mistake, setMistake] = useState(0)
   useEffect(() => {
     const handleResize = () => {
       const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
@@ -34,98 +25,7 @@ const Page = ({ params }) => {
     };
   }, []);
 
-  useEffect(() => {
-    const apiFetch = async () => {
-      const url = process.env.NEXT_PUBLIC_BACKEND;
-      const apiurl = url + '/typingpassage/' + params.practice[0];
-      let res = await fetch(apiurl);
-      let data = await res.json();
-      if (data.status === 200) {
-        const passage=data.details.passage_content
-        // console.log(data)
-        let result = passage.map(word => word.split('').map(char => char !== ' ' ? [char] : [' ']));
-        // console.log(result)
-        setpassage(result);
-        // sessionStorage.setItem('mistake',0);
-        // sessionStorage.setItem('charcounter',0); // columncounter
-        // sessionStorage.setItem('wordcounter',0);
-        // sessionStorage.setItem('rowcounter',0);
-      }
-    };
-    apiFetch();
-  }, [donotreload]);
-  const handleKeyDown=()=>{
-    // const allowedKeys = /^[0-9a-zA-Z*&^%$#@!\b]+$/;
-    const excludedKeys = /^(Control|Shift|Alt|F\d+|Tab|CapsLock|ScrollLock|NumLock|Enter|Insert|Delete|PrintScreen|Escape)$/;
-    if(!excludedKeys.test(e.key)){
-      console.log("allowed")
-      // if (e.key === 'Shift' || e.key === 'Control') {
-      //   return;
-      // }
-      if(e.key==='Backscape'){
-        if((passage[rowcounter][charcounter]===" ") || (passage[rowcounter][charcounter-1] === " ") || (charcounter==0)){
-          // console.log(3)
-        }
-        else{
-          setCharcounter(charcounter-1);
-          let character=passage[rowcounter][charcounter];
-          character=character.classList.add('highlight');
-          let result=passage;
-          result[rowcounter][charcounter]=character;
-          setpassage(result);
-        }
-      }
-      else{
-          console.log(rowcounter,charcounter)
-          let character=passage[rowcounter][charcounter];
-          character=character.classList.add('highlight');
-          let result=passage;
-          result[rowcounter][charcounter]=character;
-          setpassage(result);
-          if(e.key !== passage[rowcounter][charcounter]){
-          setMistake(mistake+1);
-          // change color of the word
-          if(passage[rowcounter][charcounter]===" ");
-          else{
-            let character=passage[rowcounter][charcounter];
-            character=character.classList.add('mainbodywrong');
-            let result=passage;
-            result[rowcounter][charcounter]=character;
-            setpassage(result);
-          }
-        }
-        if (e.key==" " && passage[rowcounter][charcounter] === " ") setWordcounter(wordcounter+1);
-        if(!passage[rowcounter][charcounter+1]){
-          setWordcounter(wordcounter+1);
-          if(!passage[rowcounter][charcounter+2]){
-            // submit the test .. no further paragraph
-          }
-          else{
-            setRowcounter(rowcounter+1);
-            setCharcounter(-1)
-            setwhichpassage(whichpassage)
-          }
-        } 
-        setCharcounter(charcounter+1);
-      }
-    }
-    else return;
-  }
-  useEffect(()=>{
-    const getpassage=()=>{
-      let result=passage;
-      console.log(result);
-      setparticularpassage(passage[whichpassage])
-    }
-    getpassage();
-  },[whichpassage,passage])
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
 
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [passage]);
   return showContent ? (
     <div className="mainparacontainer">
       <div className="mainparanavbar">
@@ -141,7 +41,7 @@ const Page = ({ params }) => {
       </div>
       <div className="mainbody">
         {/* {passage.passage_content} */}
-        {particularpassage}
+        {/* {particularpassage} */}
       </div>
     </div>
   ) : (
